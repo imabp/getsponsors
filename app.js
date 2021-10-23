bb286b221923f72e9a010e719032238e2fedf295
Prathamesh-Shanbhag <prathameshshanbhag074@gmail.com>
2021-10-23 16:20:07 +0530

    fix:Display None on Click :bug:

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const sponsor_Text = document.getElementById('spon_text');
+const sponsor_Text = document.querySelector('.sponsor_text');
+const sponsor_Text2 = document.querySelector('.sponsor_text2');
 const blob = document.querySelector('.blob');
+const blob2 = document.querySelector('.blob2');
 
 sponsor_Text.addEventListener('click', () => {
   blob.classList.add('fill_animation');
 });
+sponsor_Text2.addEventListener('click', () => {
+  blob2.classList.add('fill_animation');
+});
+
 sponsor_Text.addEventListener('click', () => {
-  blob.classList.remove('blob:hover');
+  sponsor_Text.classList.add('spon_textdis');
+});
+sponsor_Text2.addEventListener('click', () => {
+  sponsor_Text2.classList.add('spon_textdis');
 });
