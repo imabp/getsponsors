6066362758683cef9e32effe2e8b1fcb09599ae8
Prathamesh-Shanbhag <prathameshshanbhag074@gmail.com>
2021-10-23 17:36:51 +0530

    feat:Animation :sparkles:

--- js/app.js
+++ js/app.js
@@ -3,6 +3,28 @@ const sponsor_Text2 = document.querySelector('.sponsor_text2');
 const blob = document.querySelector('.blob');
 const blob2 = document.querySelector('.blob2');
 
+var animateScaling = anime({
+  targets: '.blob',
+  keyframes: [{ scale: 0.01 }, { scale: 1 }],
+  borderRadius: ['0%', '120%'],
+  rotate: '3turn',
+  easing: 'easeInOutQuad',
+  duration: 5000,
+  autoplay: false,
+});
+var animateScaling2 = anime({
+  targets: '.blob2',
+  keyframes: [{ scale: 0.01 }, { scale: 1 }],
+  borderRadius: ['0%', '120%'],
+  rotate: '3turn',
+  easing: 'easeInOutQuad',
+  duration: 5000,
+  autoplay: false,
+});
+
+document.querySelector('.play-scale').onclick = animateScaling.play;
+document.querySelector('.play-scale2').onclick = animateScaling2.play;
+
 sponsor_Text.addEventListener('click', () => {
   blob.classList.add('fill_animation');
 });
