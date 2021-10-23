05b569d2ac1490898be9dd46239a3999dd45a19d
Abir <abir.p@affinidi.com>
2021-10-23 21:57:43 +0530

    add

--- utils/storyblok.js
+++ utils/storyblok.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import StoryblokClient from "storyblok-js-client";
 
 const Storyblok = new StoryblokClient({
-  accessToken: "YOUR_PREVIEW_TOKEN",
+  accessToken:process.env.STORY_BLOCK_PREVIEW_TOKEN,
   cache: {
     clear: "auto",
     type: "memory",
